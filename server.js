2196d3f03554e86883ac0b6d856d542bca805cc0
Quynh Nga Nguyen <qn281091@gmail.com>
2015-06-19 04:42:07 -0700

    added database and displayed chat history when logging in

--- server.js
+++ server.js
@@ -1,52 +1,64 @@
 var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-//var bodyParser = require('body-parser');
-//app.use(bodyParser.urlencoded({
-//	extended: true
-//}));
+var fs = require('fs');
+var dbfile = "database.db";
+var existed = fs.existsSync(dbfile); 
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(dbfile);
 
 app.get("/", function(req, res) {
 	res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/chatroom", function(req, res) {
-	//var name = req.params.name;
-	//console.log(name);
 	res.sendFile(__dirname + "/chatroom.html");
-	// TODO 
+	// TODO: get chatroom before enter name
 });
 
 app.get("/style.css", function(req, res) {
 	res.sendFile(__dirname + "/style.css");
 });
 
-/*app.post("/chatroom", function(req, res) {
-	var name = req.body.name;
-	res.sendFile(__dirname + "/chatroom.html");
-	io.sockets.emit("newUser", name);
-});*/
-
-io.on("connection", function(socket) {
-	socket.on("join", function(name) {
-		socket.nickname = name; // sets the name associated with this client
-		socket.broadcast.emit("newUser", name);
-		socket.emit("newUser", name);
-	});
-
-	socket.on("message", function(message) {
-		var name = socket.nickname;
-		var time = getTimeString();
-		socket.broadcast.emit("newMessage", name, message, time);
-		socket.emit("newMessage", name, message, time);
-	});
-
-	socket.on("disconnect", function(name) {
-		socket.broadcast.emit("exit", name);
-	});
+db.serialize(function() {
+	if (!existed) {
+		db.run("CREATE TABLE chatHistory (name TEXT, message TEXT, time TEXT)");
+	}
 });
 
-server.listen(8080);
+io.on("connection", function(socket) {
+	//TODO: handle name existed case
+	socket.on("join", function(name) {
+		socket.nickname = name; // sets the name associated with this client
+		// announces new user
+		socket.broadcast.emit("newUser", name);
+		socket.emit("newUser", name);
+
+		// loads chat history
+		db.serialize(function() {
+			if (existed) {
+				db.each("SELECT * FROM chatHistory", function(err, row) {
+					socket.emit("message", row.name, row.message, row.time);
+				});
+			}
+		});
+	});
+
+	socket.on("newMessage", function(message) {
+		var time = getTimeString();
+		socket.broadcast.emit("message", socket.nickname, message, time);
+		socket.emit("message", socket.nickname, message, time);
+		// saves message into database
+		db.serialize(function() {
+			db.run("INSERT into chatHistory (name, message, time)" +
+				" VALUES (?, ?, ?)", socket.nickname, message, time);
+		});
+	});
+
+	socket.on("disconnect", function() {
+		socket.broadcast.emit("exit", socket.nickname);
+	});
+});
 
 // TODO: format
 function getTimeString() {
@@ -64,3 +76,6 @@ function getTimeString() {
 
 	return ret;
 }
+
+server.listen(8080);
+//db.close();
